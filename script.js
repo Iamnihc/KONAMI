674555bd9e522392ff5e077787f6e293196d4724
Iamnihc <34426008+Iamnihc@users.noreply.github.com>
2020-07-05 22:39:09 -0700

    made work

---
+++ script.js
@@ -0,0 +1,59 @@
+stage=0;
+window.addEventListener("keydown", function(event) {
+    if (stage==0&& event.key=="ArrowUp"){
+        stage++;
+        console.log(stage);
+    }
+    else if (stage==1&&event.key=="ArrowUp"){
+        stage++;
+        console.log(stage);
+    }
+    else if (stage==2&&event.key=="ArrowDown"){
+        stage++;
+        console.log(stage);
+    }
+    else if (stage==3&&event.key=="ArrowDown"){
+        stage++;
+        console.log(stage);
+    }
+    else if (stage==4&&event.key=="ArrowLeft"){
+        stage++;
+        console.log(stage);
+    }
+    else if (stage==5&&event.key=="ArrowRight"){
+        stage++;
+        console.log(stage);
+    }
+    else if (stage==6&&event.key=="ArrowLeft"){
+        stage++;
+        console.log(stage);
+    }
+    else if (stage==7&&event.key=="ArrowRight"){
+        stage++;
+        console.log(stage);
+    }
+    else if (stage==8&&event.key=="b"){
+        stage++;
+        console.log(stage);
+    }
+    else if (stage==9&&event.key=="a"){
+        stage++;
+        console.log(stage);
+    }
+    else if (stage==10&&event.key=="Enter"){
+        stage++;
+        console.log(stage);
+        console.log("complete");
+        document.getElementById("in").hidden=false;
+    }
+    else if (stage==11 && )
+    else{
+        stage=0;
+        console.log(event.key=="Enter");
+    }
+
+
+  }, true);
+
+  
+
